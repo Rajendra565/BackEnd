2a1807e50e15f8377d4bcf9b336baa4377b375b8
Rajendra565 <badatyarajendra186@gmail.com>
2025-02-21 23:27:06 +0530

    Today one more concept Readline module

---
+++ NodeJS/Mini_project/app.js
@@ -0,0 +1,48 @@
+
+import readline from 'readline'
+
+const rl=readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+})
+
+const todo=[]
+const showMen=()=>{
+  console.log('\n 1:Add a Task') 
+  console.log(' 2: view Tasks')
+  console.log(' 3: Exit')
+  rl.question('Choose an options:', handleInput)
+}
+const handleInput=(option)=>{
+  if(option === '1'){
+    rl.question("Enter the Task : ",(task)=>{
+      todo.push(task)
+      console.log("Task added: ",task);
+      showMen()
+    })
+  }
+ else if(option==='2'){
+    console.log("\n Your Todo Lists");
+    if(todo.length==0){
+      console.log(" Your todo is empty")
+    }
+
+    todo.forEach((task,index)=>{
+      console.log(`${index+1}.${task}`)
+    })
+    showMen()
+    
+  }
+  else if(option==='3'){
+    console.log("Good byee see you again")
+    rl.close()
+
+  }
+  else{
+    console.log("Invalid Option. Please try again");
+    showMen()
+
+  }
+}
+
+showMen()
