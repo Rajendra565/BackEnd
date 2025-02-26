7037709c37d2ec759166bc0961ccc1f2332f3c46
Rajendra565 <badatyarajendra186@gmail.com>
2025-02-27 00:23:48 +0530

    Add more project

---
+++ NodeJS/Mini_project/fileCreateCli/fileCreate.js
@@ -0,0 +1,16 @@
+import readline from 'readline';
+import fs from 'fs'
+
+const rl=readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+})
+function name(){
+rl.question("Rajendra: ",headling)
+}
+function headling(option){
+  if(option==1){
+    
+  }
+}
+console.log(name())
---
+++ hello.js
@@ -0,0 +1,6 @@
+let arr=[12,40,7]
+arr[10]=10
+
+console.log(arr[9]=4)
+
+
